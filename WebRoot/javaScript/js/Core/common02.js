c0667169d8c6a1fd46bfa4c01237409f5e84e5d8
cuicuiworld <407060373@qq.com>
2017-04-28 17:50:22 +0800

    date

--- WebRoot/javaScript/js/Core/common02.js
+++ WebRoot/javaScript/js/Core/common02.js
@@ -292,4 +292,80 @@ function getCheckCount(chkName){
    return $('input[@type=checkbox][@name='+chkName+'][@checked]').length;
 }
 
+/**
+ * 控件enter事件
+ * 
+ * @param parentId
+ * 				父级id或class
+ * @param submitId
+ * 				提交按钮id或class
+ */
+function enterSubmit(parentId,submitId){
+	$(parentId).keydown(function(e){
+		if(e.keyCode==13){
+			$(submitId).click();
+		}
+	});
+}
 
+/**
+ * 获取当前时间
+ * 
+ * 根据需要确定是否精确到秒
+ * 
+ * @returns {String}
+ */
+function getNowFormatDate() {
+    var date = new Date();
+    var seperator1 = "-";
+    var seperator2 = ":";
+    var month = date.getMonth() + 1;
+    var strDate = date.getDate();
+    if (month >= 1 && month <= 9) {
+        month = "0" + month;
+    }
+    if (strDate >= 0 && strDate <= 9) {
+        strDate = "0" + strDate;
+    }
+    var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate
+            + " " + date.getHours() + seperator2 + date.getMinutes()
+            + seperator2 + date.getSeconds();
+    return currentdate;
+}
+/** 
+ * 依据当前月份，获取改变的时间，
+ * 
+ * 获取下一个月 ，上一个月，下一个季度，下半年，一年
+ * 
+ * @date 格式为yyyy-mm-dd的日期，如：2014-01-25 
+ */ 
+function getChangeDate(date,num) {  
+    var arr = date.split('-');  
+    var year = arr[0]; //获取当前日期的年份  
+    var month = arr[1]; //获取当前日期的月份  
+    var day = arr[2]; //获取当前日期的日  
+    var days = new Date(year, month, 0);  
+    days = days.getDate(); //获取当前日期中的月的天数  
+    var year2 = year;  
+    var month2 = parseInt(month) + Number(num);  
+    if (month2 > 12) {  
+        year2 = parseInt(year2) + 1;  
+        month2 = 1;  	
+    }
+    if(month2 < 1 ){
+    	year2 = parseInt(year2) - 1;
+    	month2 = 12;
+    }
+    var day2 = day;  
+    var days2 = new Date(year2, month2, 0);  
+    days2 = days2.getDate();  
+    if (day2 > days2) {  
+        day2 = days2;  
+    }  
+    if (month2 < 10) {  
+        month2 = '0' + month2;  
+    }  
+  
+    var t2 = year2 + '-' + month2 + '-' + day2;  
+    return t2;  
+} 
